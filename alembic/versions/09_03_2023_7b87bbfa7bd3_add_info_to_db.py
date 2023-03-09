5c60bdde12e9f950c8c4b6373ab83b2ddda7a3b1
Vismut <pavel.epanovm@gmail.com>
2023-03-09 21:39:37 +0500

    Add endpoints: id/rooms and /location - should change

---
+++ alembic/versions/09_03_2023_7b87bbfa7bd3_add_info_to_db.py
@@ -0,0 +1,24 @@
+"""Add info to DB
+
+Revision ID: 7b87bbfa7bd3
+Revises: a3ef613e39b8
+Create Date: 2023-03-09 20:23:40.906259
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7b87bbfa7bd3'
+down_revision = 'a3ef613e39b8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- src/hotels/routers.py
+++ src/hotels/routers.py
@@ -4,7 +4,7 @@ from database import get_async_session
 from repository.base import BaseRepo
 from fastapi import Depends
 
-from hotels.models import Hotel
+from hotels.models import Hotel, Room
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -13,8 +13,22 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/{location}")
+async def get_all_hotels_with_date_from_to_location(location: str, session: AsyncSession = Depends(get_async_session)):
+    repo = BaseRepo(Hotel)
+    result = await repo.get_hotels_by_location(location, session)
+    return result
+
+
+@router.get("/{id}")
 async def get_hotel_info_with_id(id: int, session: AsyncSession = Depends(get_async_session)):
     repo = BaseRepo(Hotel)
     result = await repo.get_by_id(id, session)
+    return result
+
+
+@router.get("/{id}/rooms")
+async def get_rooms_by_hotel(id: int, session: AsyncSession = Depends(get_async_session)):
+    repo = BaseRepo(Room)
+    result = await repo.get_rooms_by_id_hotel(id, session)
     return result
